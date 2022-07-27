f9448989dce91f9f3d7aab5cad32413dcbcfb607
Leandro Marcomini <38842048+leandromarcomini@users.noreply.github.com>
2022-07-27 16:08:53 -0300

    Updates conversion script

--- mAP/mAP_yolo/input/Yolo/Convert_CSV_txt.py
+++ mAP/mAP_yolo/input/Yolo/Convert_CSV_txt.py
@@ -7,10 +7,9 @@ with open(INPUT_FILE, 'r') as csvfile:
     reader = DictReader(csvfile)
     for row in reader:
         txt = os.path.splitext(row["image"])[0] 
-        print("txt" + txt)
         file_name = "{}.txt".format(txt)
-        print("filename" + file_name)
-        line = "Eixo " + row["confidence"] + " " + str(round(float(row["xmin"]))) + " " + str(round(float(row["ymin"]))) + " " + str(round(float(row["xmax"]))) + " " + str(round(float(row["ymax"]))) +'\n'
+        print('Processing', file_name)
+        line = "Axle " + row["confidence"] + " " + str(round(float(row["xmin"]))) + " " + str(round(float(row["ymin"]))) + " " + str(round(float(row["xmax"]))) + " " + str(round(float(row["ymax"]))) +'\n'
         with open(file_name, 'a') as output:
             output.write(line)
 
